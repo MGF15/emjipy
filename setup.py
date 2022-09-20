e89d44da8cc3f4e93df8426082bf4fcaf57cbf98
MGF15 <mohamed5548@live.com>
2022-09-20 13:32:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='emojipy',
-    packages=['emojipy'],
-    version='1.0.2',
+    name='emjipy',
+    packages=['emjipy'],
+    version='1.0.3.1',
     description='emoji for Python',
     author='MGF15',
     license='MIT',
