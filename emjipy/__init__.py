e380aa0a0636652806182203a2f0e9f7d7c71d80
MGF15 <mohamed5548@live.com>
2022-09-20 13:31:24 +0200

    Add files via upload

---
+++ emjipy/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+from emjipy.emjipy import *
+from emjipy.unicode import *
+
+__all__ = [
+    'emoji',
+    'emoji_name',
+    'emoji_unicode',
+    'version'
+
+]
+
+__version__ = '1.0.3'
+__author__ = 'MGF15'
+__source__ = 'https://github.com/MGF15/emjipy/'
